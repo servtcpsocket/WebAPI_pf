0361ba584c0ab2c3c18f4c2fdc48340f76783c1d
Antonio M. Amaya <amac@peregrin>
2015-05-06 17:20:51 +0200

    Test infrastructure set up, take 1

---
+++ tests/settings.js
@@ -0,0 +1,47 @@
+// mozSettings API tests (polyfilled!)
+(function(window) {
+  window.Tests = window.Tests || {};
+
+  var dependencies = ['/WebAPI_pf/polyfills/settings/settings.js'];
+
+  window.Tests['settings'] =
+    LazyLoader.load(dependencies).then(() => {
+      var log = window.Tests.log.bind(undefined, 'settings');
+      return {
+        runTest: function() {
+          function abort(e) {
+            throw e;
+          }
+
+          try {
+            log('Starting settings polyfill tests');
+            window.mozSettings || abort('window.mozSettings not defined.');
+
+            log('window.mozSettings defined!');
+            var lock = window.mozSettings.createLock();
+
+            lock && log('We got a lock!') &&
+              (lock.serialize && log('And it\'s fake!')) ||
+              abort('And it\'s a real one... Done!');
+
+            // Going to kill two stones with a bird. Or something... :P
+            window.mozSettings.setObserver('i.am.a.setting', function(e) {
+              log('Got a event for my setting: ' + JSON.stringify(e));
+            });
+
+            lock.set('i.am.a.setting', 'abcd1234').then(() => {
+              log('Setting set! (hopefully)');
+            });
+            lock.get('i.am.a.setting').then(e => {
+              log('Setting read! ' + JSON.stringify(e));
+            });
+
+          } catch (e) {
+            log("Finished early with " + e);
+          }
+
+        }
+      };
+    });
+
+})(window);
---
+++ tests/tests.js
@@ -0,0 +1,41 @@
+(function(window) {
+
+  'use strict';
+
+  // So far tests are going just to write on console.log... if we want something
+  // nicer this would be a good place to add it...
+  window.Tests = {
+    log: function(test, text) {
+      console.log('--**-- ' + test + ': ' + text);
+      return true; // So we can chain this...
+    }
+  };
+
+  function runTest(serviceToTest) {
+    // When the js loads, window.Tests[xxx] holds a promise
+    // that fulfills when the dependencies have been loaded. So...
+    LazyLoader.load([serviceToTest + '.js']).
+      then(() => window.Tests[serviceToTest]).
+      then(test => test.runTest());
+  }
+
+  window.addEventListener('load', function () {
+    // Hopefully this will be false at some point in life...
+    var loadNavConnectPolyfill = !!navigator.connect;
+    var allLoaded;
+    if (loadNavConnectPolyfill) {
+      allLoaded = LazyLoader.
+                    load(['https://raw.githubusercontent.com/mcjimenez/' +
+                          'nav_connect/usemessagechannel/client/' +
+                          'navigator_connect.js']);
+    } else {
+      allLoaded = Promise.resolve();
+    }
+    allLoaded.then(() => {
+      var settings = document.querySelector('#settings');
+      var tcpsocket = document.querySelector('#tcpsocket');
+      settings.addEventListener('click', runTest.bind(undefined, 'settings'));
+      tcpsocket.addEventListener('click', runTest.bind(undefined, tcpsocket'));
+    });
+  });
+})(window);
