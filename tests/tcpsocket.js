b4c7f6f522cac0971e179e520cf6d8997f0320f3
Antonio M. Amaya <amac@tid.es>
2015-05-13 11:33:55 +0200

    Set local tcpsocket test

--- tests/tcpsocket.js
+++ tests/tcpsocket.js
@@ -20,7 +20,7 @@
         var _mozTCPSocket = window.navigator.mozTCPSocket;
 
         log('window.navigator.mozTCPSocket defined!');
-        var host = '192.168.1.1';
+        var host = '192.168.2.24';
         var port = 12345;
         var options = {};
         var socket = _mozTCPSocket.open(host, port, options);
