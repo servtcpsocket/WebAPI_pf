fa6f93c0e1ac5a98e8a2e09799758af355fde416
Antonio M. Amaya <amac@peregrin>
2015-05-25 14:57:56 +0200

    Fixed IP address

--- tests/tcpsocket.js
+++ tests/tcpsocket.js
@@ -20,7 +20,7 @@
         var _mozTCPSocket = window.navigator.mozTCPSocket;
 
         log('window.navigator.mozTCPSocket defined!');
-        var host = '192.168.1.10';
+        var host = '192.168.1.38';
         var port = 12345;
         var options = {};
         log('Starting test');
